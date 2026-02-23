142b83f7af736ddc6fb368fd9989254f80039de7
Eric Chen <ericchenbuffet@gmail.com>
2026-02-22 20:53:59 -0600

    Modify dryness threshold and enhance code comments
    Updated dryness threshold for moisture sensor and added comments for clarity.

--- arduino IOT code/main.ino
+++ arduino IOT code/main.ino
@@ -5,8 +5,9 @@
 #define PIN_RELAY_4  19 
 #define AOUT_PIN1 34 
 #define AOUT_PIN2 35 
-
-#define DRYNESS 2200 // ESP32 pin GPIO34 (ADC0) that connects to AOUT pin of moisture sensor
+//threshold value that we impose as the programmer, change the value higher for dryer soil.
+//ensure the sensor is seated deeply in the soil, otherwise readings won't be accerate
+#define DRYNESS 2800 
 const int FLOATSENSOR = 12;
 bool float_Status = true;
 
@@ -21,38 +22,38 @@ void setup() {
 
   initProperties();
 
-  ArduinoCloud.begin(ArduinoIoTPreferredConnection);
-  setDebugMessageLevel(2);
-  ArduinoCloud.printDebugInfo();
+  ArduinoCloud.begin(ArduinoIoTPreferredConnection);  // IOT libraries
+  setDebugMessageLevel(2);  // IOT libraries
+  ArduinoCloud.printDebugInfo();  // IOT libraries
 }
 void loop() {
 
 
 
-  float_Status = digitalRead(FLOATSENSOR);
+  float_Status = digitalRead(FLOATSENSOR); //Reads signal
 
   if (float_Status == HIGH){
     float_Sensor = true;
   }else {
     float_Sensor = false;
   }
-// these should be wrapped into a function, having code like this is not great
-// classes aren't neeeded but I will think about maybe writing one if time permits
 
   int value1 = analogRead(AOUT_PIN1); // read the analog value from sensor;
   
+   //If the code doesn't work change flip the LOW and HIGH 
   if ((value1 > DRYNESS) && (float_Sensor == true)){
   digitalWrite(PIN_RELAY_1, LOW);
+  delay(3000);
   }else{
   digitalWrite(PIN_RELAY_1, HIGH);
   }
 
 
-// class/function
   int value2 = analogRead(AOUT_PIN2); // read the analog value from sensor
   
   if ((value2 > DRYNESS) && (float_Sensor == true)){
   digitalWrite(PIN_RELAY_2, LOW);
+  delay(3000);
   }else{
   digitalWrite(PIN_RELAY_2, HIGH);
   }
@@ -66,6 +67,7 @@ void loop() {
  }
 
 void onWaterOn1Change() {
+  // The code that activiates when you push the buttons on the dashboard.
   if (water_On_1) {
     digitalWrite(PIN_RELAY_1, LOW);
     delay(3000);
@@ -75,6 +77,7 @@ void onWaterOn1Change() {
 }
 
 void onWaterOn2Change() {
+  // The code that activiates when you push the buttons on the dashboard.
   if (water_On_2) {
     digitalWrite(PIN_RELAY_2, LOW);
     delay(3000);
